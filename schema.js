3f752f12fad9b59a77147f6652915766186f9950
Om <omspatil980@gmail.com>
2024-10-04 18:05:13 +0530

    add project files

---
+++ schema.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+
+
+listingSchema = Joi.object({
+
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    category: Joi.string().valid('Trending', 'Rooms', 'Iconic Cities', 'Mountain', 'Castles', 'Amazing Pools', 'Campings', 'Farms', 'Arctic', 'Doms', 'Boats').required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price:Joi.number().required().min(0),
+    image: Joi.object({
+        url: Joi.string().uri().allow('', null) // Allow empty strings or null
+    }).allow(null)
+
+})
+
+const reviewSchema =  Joi.object({
+
+
+        rating:Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+
+
+});
+
+module.exports = { listingSchema,reviewSchema };
