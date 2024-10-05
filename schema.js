00db43187107765cf2c5637ac92fed52f9f72220
OM PATIL <65182929+om151@users.noreply.github.com>
2024-10-05 11:32:44 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -7,7 +7,7 @@ listingSchema = Joi.object({
     description: Joi.string().required(),
     location: Joi.string().required(),
     country: Joi.string().required(),
-     category: Joi.string().required(),
+      category: Joi.string().valid('Trending', 'Rooms', 'Cities', 'Mountain', 'Castles', 'Pools', 'Campings', 'Farms', 'Arctic', 'Doms', 'Boats').required(),
     price:Joi.number().required().min(0),
     image: Joi.object({
         url: Joi.string().uri().allow('', null) // Allow empty strings or null
